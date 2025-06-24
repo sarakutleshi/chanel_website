6925ca642fa7c62f8374abf42bc83d2b0d4dbfb2
sarakutleshi <Sara.kutleshi@student.cacttus.education>
2025-06-24 17:08:24 +0200

    Refactor MakeUpAndSkinCare section: Remove index.html, update makeUp.html links, and clean up CSS styles

--- HomePage/main.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded" ,
-    function () {
-        const quoteElement = document.getElementById("quote");
-        quoteElement.addEventListener("click", function () {
-            alert("Quote clicked!");
-        });
-    });
